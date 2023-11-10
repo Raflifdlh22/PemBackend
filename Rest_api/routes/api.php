7166e15907f60a43e1b76bec496115d35841e782
Raflifdlh22 <raflifdlh2@gmail.com>
2023-11-10 11:22:25 +0700

    authentication

--- Rest_api/routes/api.php
+++ Rest_api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,39 +21,47 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route untuk menampilkan seluruh semua hewan
-Route::get('/animals', [AnimalController::class, "index"]);
+Route::middleware('auth:sanctum')->Group(function () {
+    // Route untuk menampilkan seluruh semua hewan
+    Route::get('/animals', [AnimalController::class, "index"]);
 
-// Route untuk menambahkan hewan
-Route::post('/animals', [AnimalController::class, "store"]);
+    // Route untuk menambahkan hewan
+    Route::post('/animals', [AnimalController::class, "store"]);
 
-// Route untuk mengedit hewan
-Route::put('/animals/{id}', [AnimalController::class, "update"]);
+    // Route untuk mengedit hewan
+    Route::put('/animals/{id}', [AnimalController::class, "update"]);
 
-// Route untuk menghapus hewan
-Route::delete('/animals/{id}', [AnimalController::class, "destroy"]);
+    // Route untuk menghapus hewan
+    Route::delete('/animals/{id}', [AnimalController::class, "destroy"]);
 
-// Route untuk menampilkan seluruh semua students
-Route::get('/students', [StudentsController::class, 'index']);
+    // Route untuk menampilkan seluruh semua students
+    Route::get('/students', [StudentsController::class, 'index']);
 
-// Route untuk menambahkan students
-Route::post('/students', [StudentsController::class, 'store']);
+    // Route untuk menambahkan students
+    Route::post('/students', [StudentsController::class, 'store']);
 
-// Route untuk mengedit students
-Route::put('/students/{id}', [StudentsController::class, 'update']);
+    // Route untuk mengedit students
+    Route::put('/students/{id}', [StudentsController::class, 'update']);
 
-// Route untuk menghapus students
+    // Route untuk menghapus students
 
-Route::delete('/students/{id}', [StudentsController::class, 'destroy']);
+    Route::delete('/students/{id}', [StudentsController::class, 'destroy']);
 
-// Route untuk menampilkan seluruh semua students
-Route::get('/students{id}', [StudentsController::class, 'show']);
+    // Route untuk menampilkan seluruh semua students
+    Route::get('/students{id}', [StudentsController::class, 'show']);
 
-// Route untuk update students
-Route::put('/students/{id}', [StudentsController::class, 'update']);
+    // Route untuk update students
+    Route::put('/students/{id}', [StudentsController::class, 'update']);
 
-// Route untuk menambah students
-Route::post('/students', [StudentsController::class, 'store']);
+    // Route untuk menambah students
+    Route::post('/students', [StudentsController::class, 'store']);
 
-// Route untuk untuk mendapatkan detail student
-Route::get('/students/{id}', [StudentsController::class, 'show']);
+    // Route untuk untuk mendapatkan detail student
+    Route::get('/students/{id}', [StudentsController::class, 'show']);
+});
+
+
+
+// Route untuk autentikasi Register & Login
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
